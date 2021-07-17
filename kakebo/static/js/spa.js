8ca6969423410c72c6c5e4531bb5708b1e935958
Marcos Alonso <marcosalonso@MacBook-Pro.home>
2021-07-17 10:12:14 +0200

    Creando APIs  por peticiones, incluidas GET y PUT

--- kakebo/static/js/spa.js
+++ kakebo/static/js/spa.js
@@ -9,6 +9,18 @@ const categorias = { //Creo el objeto categorias en forma de diccionario, si bie
 let losMovimientos  //Creo la variable losMvimientos vacia en el ambito global
 
 
+function recibeRespuesta() {
+    if (this.readyState === 4 && this.status === 200) {
+        const respuesta = JSON.parse(this.responseText)
+
+        if (respuesta.status !== "success") {
+            alert("Se ha producido un error en acceso a servidor" + respuesta.mensaje)
+            return
+        }
+
+       llamaApiMovimientos() //Este llamaApiMovimientos va a hacer lo mismo que el onload, va a crear la peticion 'GET', `http://localhost:5000/api/v1/movimientos`, la va a lanzar, cuando llegue, muestraMovimientos va a volver a mostrar la lista. Lo que no hace muestraMovimientos es limpiar la lista, asi qeu lo vamos a hacer quitando el const tbody de donde estaba
+    }
+}
 
 function detallaMovimiento(id) {
 
@@ -23,6 +35,7 @@ function detallaMovimiento(id) {
     }
     if (!movimiento) return 
 
+    document.querySelector("#idmovimiento").value = id //Esto sirve para guardar el id de cada movimiento, se guarda en la funcion detallaMovimiento y se recupera cuando se hace click en ese registro, en caso de que lo hagamos para modificarlo, para pasar los datos al formulaio de detalle de movimiento, aunq el id no se va a mostrar, ya que se ha creado como un campo oculto en el HTML
     document.querySelector("#fecha").value = movimiento.fecha  //Este movmiento.fecha es el valor del item.fecha como se ha definido en el bloque de for arriba
     document.querySelector("#concepto").value = movimiento.concepto
     document.querySelector("#categoria").value = movimiento.categoria
@@ -44,7 +57,9 @@ function muestraMovimientos() {
         }
 
         losMovimientos = respuesta.movimientos //Guardo los movimientos en la variable los mOVIMIENTOS EN LA MEMORIA DE LA PAGINA, al asignarle el vaLor respuesta.movimientos. Ya puedo acceder a ellos desde el forntend desde el id.
-        
+        const tbody = document.querySelector(".tabla-movimientos tbody") //Creo la constante tbody, donde se guardan los datos que aparecen en la const dentro. Elijo el punto del DOM donde quiero meter estos datos, que es en el tbody del fichero spa.html. Se separa por un espacio porque es un selector de CSS combinador de descendientes
+        //Meto ahora aqui la constante tbody para que limpie la lista de movimientos y la vuelva a cargar cuando hagamos una modifciación de la tabla de movimientos
+        tbody.innerHTML = "" 
         for (let i=0; i < respuesta.movimientos.length; i++) { //Recorro todas las respuestas obtenidas con la peticion, mientras la respuesta tenga movimientos que mostrar, es decir, mientras respuesta.movimientos.length
             const movimiento = respuesta.movimientos[i] //Se crea la variable movimiento para guardar cada uno de los movimientos que se muestran como consecuencia de la respuesta de la petición a los movimientos de la base de datos
             
@@ -62,7 +77,7 @@ function muestraMovimientos() {
                 <td>${movimiento.cantidad} €</td>
             `
             fila.innerHTML = dentro //Inyectamos en la fila los datos que correspondan a cada registro segun los hemos indicado en la constante dentro
-            const tbody = document.querySelector(".tabla-movimientos tbody") //Creo la constante tbody, donde se guardan los datos que aparecen en la const dentro. Elijo el punto del DOM donde quiero meter estos datos, que es en el tbody del fichero spa.html. Se separa por un espacio porque es un selector de CSS combinador de descendientes
+            
             tbody.appendChild(fila) //Se ubica dentro de cada fila que se crea, de cada <tr>
         
         }
@@ -70,15 +85,35 @@ function muestraMovimientos() {
 }
 
 xhr = new XMLHttpRequest() //Creo el manejador de peticiones 
-xhr.onload = muestraMovimientos //Cuando se produzca la carga de lo que devulve la petición, se va a ejecutar la funcion muestraMovimientos
+
 
 function llamaApiMovimientos() { //Esta función abre la llamada a la api de movimientos y la envia
     xhr.open('GET', `http://localhost:5000/api/v1/movimientos`, true) /*Esta funcion lo que hace es crear la url */
+    xhr.onload = muestraMovimientos //Cuando se produzca la carga de lo que devulve la petición, se va a ejecutar la funcion muestraMovimientos
     xhr.send()
 }
 
 window.onload = function() { /*Lo que haya dentro de esta funcion se va a ejecutar cuando la pagina termina de cargarse, aunque el script que hay en el fichero spa.html que hace referencia al fichero spa.js, este al principio del documento, en el head, y en teoria se ejecutaria de lo primero. Se va a ejecutar cuando la pagina este cargada, cuando este renderizada, entonces empezara a ejecutar lo que este dentro de la funcion */
     llamaApiMovimientos() /*Cuando la ventana este cargada, entonces se llama a la funcionApiMovimientos*/
     
-   
+    document.querySelector("#modificar") //Una vez cargada del todo la pagina, ya si puedo utilizar el boton modificar
+        .addEventListener("click", (ev) => { //Como quiero capar el evento de refrescar la oagina cuando pulso modificar, tengo que capturarlo, para ello lo tengo que opner el evento como parametro de la funcion, porque lo primero que quiero hacer con ese evento de refrescar es que no se produzca, pararlo
+            ev.preventDefault()   // Con el ev.preventDefault, lo que se consigue es que por defecto, no se relance la pagina, se para ese evento
+            const movimiento = {}// Ahira tengo que coger los datos que ha modificado el usuario y transformarlos para enviarlos al servidor. Para ello, creamos una const y laigualamos a un diccionario vacio {}, que es el json que voy a enviar
+            movimiento.fecha = document.querySelector("#fecha").value //En la clave de diccionario movimiento.fecha se incluira el nuevo valor introducido, si es que se ha modificado, sino guarda el que esté
+            movimiento.concepto = document.querySelector("#concepto").value
+            movimiento.categoria = document.querySelector("#categoria").value
+            movimiento.cantidad = document.querySelector("#cantidad").value
+            if (document.querySelector("#gasto").checked) { //Aqui le digo, si el id gasto (que es el radiobutton), esta checado, 
+                movimiento.esGasto = 1  //Entonces, es un gasto, debe guardarse como gasto en la base de datos
+            } else {
+                movimiento.esGasto = 0 //Y si no esta checado el boton de gasto, esGasto es igaul a 0, es decir, es un ingreso y se guardara asi en la base de datos
+            }
+            id = document.querySelector("#idmovimiento").value //Con esto lo que hago es guardar en la memoria del navegador el valor del id que sale al seleccionar un movimiento de la tabla de movimientos, lo guardo en la variable id
+            xhr.open("PUT", `http://localhost:5000/api/v1/movimiento/${id}`, true)  //Ahora lanzo la peticion con el xhr.open, igual que he hecho antes, pero aqui incluido, para que se haga este envio en caso de que queramos modificar, ya que ya no quiero que vaya a la fucnion muestraMovimientos, como hace el otro xhr.open que tengo mas arriba. Se incluye el id, que se ha guardado en la memoria en la linea anterior
+            xhr.setRequestHeader("Content-Type", "application/josn;charset=UTF-8") //Esto lo que está diciendo es: voy a mter en la  cabecera un json, para que en el archivo views del servidor sepa lo que esperar. En la cabecera viaja el tipo de informacion que va en el body, que en este caso es un json
+            xhr.onload = recibeRespuesta //Este onload, que es el punto de recuperacion de los datos, es para esta petición de PUT 
+            
+            xhr.send(JSON.stringify(movimiento))   //El metodo stringify lo que hace en este caso es coger un objeto de javascript y convertirlo en un texto que viaja al servidor, es el contrario a json.pars
+        }
 }
--- kakebo/views.py
+++ kakebo/views.py
@@ -1,7 +1,8 @@
 from kakebo import app  
 from kakebo.dataccess import DBmanager
-from flask import jsonify, render_template
+from flask import jsonify, render_template, request
 import sqlite3
+from http import HTTPStatus
 
 dbManager = DBmanager(app.config.get('DATABASE')) #La instancia dbManager va a coger del objeto config de la app la ruta de acceso al fichero de la base de datos que esta asignada a DATABASE, es decir '/data/movimientos.db'. La inyectamos en la clase DBManager al instanciarla.
 
@@ -20,3 +21,33 @@ def movimientosAPI():
 
     except sqlite3.Error as e:
         return jsonify({'status': 'fail', 'mensaje': str(e)}) #Si se produce un error en la consulta, devuelve un mensaje string con el error
+
+
+@app.route('/api/v1/movimiento/<int:id>', methods=['GET', 'PUT', 'DELETE']) #Esta ruta es para obtener movimientos, modificar (put) o borrar, utilizando el id 
+@app.route('/api/v1/movimiento', methods=['POST']) #Esta ruta es para crear nuevo movimiento, por lo que no requiere id, ya que este se le asigna la base de datos automaticante cuando se crea nuevo movimiento
+def detalleMovimiento(id=None):
+    try: #Le meto aqui el try para validacion de errores, delante de todos los request, asi pilla a todos 
+        if request.method in ('GET', 'PUT', 'DELETE'):
+            movimiento = dbManager.consultaUnaSQL("SELECT * FROM movimientos WHERE id = ?", [id]) #Aqui le digo que me seleccione todos los movimientos cuando el id sea el valor de entrada del id, pero los metes en una lista [id]
+
+        if request.method == 'GET':
+            if movimiento:
+                return jsonify({
+                "status": "success", 
+                "movimiento": movimiento
+            })
+            else: 
+                return jsonify({"status": "fail", "mensaje": "movimiento no encomtrado"}), 404 #Cuando creamos una api, Es importante poner el codigo 404 o en su defecto el HTTPStatus.NOT_FOUND si importamos previamente la libreria http y el modulo HTTPStatus cuando la petición no se encuentra 
+
+        if request.method == 'PUT':
+            dbManager.modificaTablaSQL("""
+            UPDATE movimientos 
+            SET fecha=:fecha, concepto=:concepto, esGasto=:esGasto, categoria=:categoria, cantidad=:cantidad
+            WHERE id = {}""".format(id), request.json) #Aqui lo actualizo con un diccionario, que es un json, que cuando python lo traduzca a un lenguaje que entienda, es un diccionario, con lo cual losparametros de entrada los voy a volcar igual 
+            #El valor con 2 puntos (:) es el valor del campo de entrada, lo que viene en el json y el primer fecha es el nombre del campo en  la base de datos. Se utiliza esta sintaxis porque es la forma de un diccionario, que es lo qeu necesitamos para enviarlo a la base de datos
+            #Los parametros de entrada van a ser request.json
+
+            return jsonify({"status":"success", "mensaje": "Registro modificado"})
+        
+    except sqlite3.Error as e:
+        return jsonify({"status":"fail", "mensaje": "Error en base de datos: {}".format(e)}), HTTPStatus.BAD_REQUEST
